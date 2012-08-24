d14201e91d001d3ed6785f4983d44c9c801ea64d
Adrian <adriancg2@gmail.com>
2012-08-24 11:46:40 -0500

    UI ideas for math editing

---
+++ data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h)]);}; 
+var b="rootNodes",c="pageName",d="Math Editor 8\/20\/12",e="type",f="Wireframe",g="url",h="Math_Editor_8_20_12.html";
+return _creator();
+})();
